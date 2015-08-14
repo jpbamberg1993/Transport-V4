6dd883a01f9c6d7226660d4af8945d306d0b8d82
Chris <chrisalan313@gmail.com>
2015-08-14 13:09:18 -0400

    Add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :offers
   resources :shipments
   devise_for :users
+
+  root 'shipments#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-wyncode = User.create( company_name: "Wyncode" )
-juvenal = User.create( company_name: "Juvenal" )
+wyncode = User.create( company_name: "Wyncode", email: "wyncode@example.com", encrypted_password: "password" )
+juvenal = User.create( company_name: "Juvenal", email: "juvenal@example.com", encrypted_password: "password" )
 
-shipment = Shipment.create( start_point: "FL", end_point: "GA", user_id: wyncode.id )
+shipment = Shipment.create( origin: "FL", destination: "GA", user_id: wyncode.id )
 
 Offer.create( amount: 10000, user_id: juvenal.id, shipment_id: shipment.id )
