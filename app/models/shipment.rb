fb084bd4df98ff2724e70c4f05d452d000b74d90
John Paul Bamberg <jpbamberg1993@gmail.com>
2015-08-14 18:57:57 -0400

    validates offer

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,33 @@
 class Shipment < ActiveRecord::Base
   belongs_to :user
   has_many :offers
+
+  validates :offer, :user, presence: :true
+  # Need to add value validation
+
+	def formatted_price 
+		price_in_dollars = price_in_cents.to_f / 100 
+		sprintf("%.2f", price_in_dollars)
+	end
 end
+
+# 1. create new controller action and use that for the admin view
+# 2. use existing controller figure out current user then create instance variable with offers if current use owns shipment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
