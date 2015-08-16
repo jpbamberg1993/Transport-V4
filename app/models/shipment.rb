74f0f0ed1eeb3d30726ee5ac6678d049b361e466
John Paul Bamberg <jpbamberg1993@gmail.com>
2015-08-16 16:15:11 -0400

    formatted price on landing page

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -11,7 +11,7 @@ class Shipment < ActiveRecord::Base
   validates :minimum_commitment, presence: :true
   validates :maximum_commitment, presence: :true
   validates :cost, presence: :true
-  validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
+  # validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
 
   def formatted_price
     price_in_dollars = cost.to_f / 100
