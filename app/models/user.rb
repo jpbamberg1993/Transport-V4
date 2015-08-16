c29d1264796918c74ddf9e809c5d00ca40e5dc8e
Chris <chrisalan313@gmail.com>
2015-08-16 13:43:07 -0400

    Add validation for User role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
-  has_many :shipments
-  has_many :offers
+  has_many :shipments, dependent: :destroy
+  has_many :offers, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # => Email, Password Already Validated
-  # validates :company_name, presence: true
+  validates :company_name, presence: true
+
+  validates_presence_of :role
 
 end
