4891982dcc746a9b76736362c5dd8616660553f7
Chris <chrisalan313@gmail.com>
2015-08-16 12:09:39 -0400

    Add broken radio buttons to user sign up. Doesn't save to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create( id: 1, email: "chrisalan313@gmail.com", password: "wyncoder", company_name: "Company 1" )
-
-User.create( id: 2, email: "example@example.com", password: "wyncoder", company_name: "Company 2")
-
-Shipment.create( id: 4, origin: "Here", destination: "There", mode_of_transportation: "Something", equipment_type: "Something", minimum_commitment: 1, maximum_commitment: 2, cost: 2222, user_id: 1 )
-
-Offer.create( id: 2, amount: 222222, user_id: 2, shipment_id: 4 )
+# User.create( id: 1, email: "chrisalan313@gmail.com", password: "wyncoder", company_name: "Company 1" )
+#
+# User.create( id: 2, email: "example@example.com", password: "wyncoder", company_name: "Company 2")
+#
+# Shipment.create( id: 4, origin: "Here", destination: "There", mode_of_transportation: "Something", equipment_type: "Something", minimum_commitment: 1, maximum_commitment: 2, cost: 2222, user_id: 1 )
+#
+# Offer.create( id: 2, amount: 222222, user_id: 2, shipment_id: 4 )
