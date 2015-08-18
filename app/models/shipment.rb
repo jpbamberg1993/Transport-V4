a8aaae799018533b011421913118c09f9a90c190
Juvenal Manikan <jmanikan@gmail.com>
2015-08-18 15:40:15 -0400

    Added validation to min/max commit. in shipment controller

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,16 +1,16 @@
 class Shipment < ActiveRecord::Base
   belongs_to :user
   has_many :offers, dependent: :destroy
-  
-  
+
+
   validates :user, presence: :true
 
   validates :origin, presence: :true
   validates :destination, presence: :true
   validates :mode_of_transportation, presence: :true
   validates :equipment_type, presence: :true
-  validates :minimum_commitment
-  validates :maximum_commitment
+  validates :minimum_commitment, presence: :true
+  validates :maximum_commitment, presence: :true
   validates :cost, presence: :true
   # validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
 
