afc3972db21af9decfa27bcc24dec6825a1c7d0b
John Paul Bamberg <jpbamberg1993@gmail.com>
2015-08-20 21:07:36 -0400

    testing shipment model

--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 describe Shipment do 
-	let(:valid_attributes) {{		
-														origin => "Orlando",
-														destination => "Boston",
-														mode_of_transportation => "Full Load",
-														equipment_type => "Van",
-														minimum_commitment=>  2,
-														maximum_commitment=>  5,
-														cost=>  50000 
-														}}
+	
 	# Checks shipment is created
 	it 'creates a shipment with valid attributes' do
 		 shipment = create(:shipment)
 		 expect(shipment).to be_valid
 	end
+	
 	# Checks Origin field is present and if not fails
 	it 'creates a shipment with in_valid attributes' do
 	 	shipment = create(:shipment)
 	 	shipment.origin = nil
 	 	expect(shipment).to be_invalid
 	end
-	
+
+	describe 'instantiation' do
+		let!(:shipment) { build(:shipment) }
+
+		it 'instantiates a shipment' do
+			expect(shipment.class.name).to eq("Shipment")
+
+		end
+	end
+
 end
