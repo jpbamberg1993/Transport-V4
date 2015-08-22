4f1f68451b6485dec8127bdb0af1570db180abfe
Juvenal Manikan <jmanikan@gmail.com>
2015-08-21 23:04:18 -0400

    Added full-title method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     current_user.role == "carrier"
   end
 
+  def full_title(page_title = '')
+    base_title = "Transportation Procurement"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
