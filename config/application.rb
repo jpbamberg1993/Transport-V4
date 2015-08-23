45b1bb7daa1b2e6beba67fbc5f6c95e964447594
Chris <chrisalan313@gmail.com>
2015-08-23 18:47:00 -0400

    More config tweeking

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module FinalV4
   class Application < Rails::Application
 
-    config.assests.initialize_on_precompile = false
+    config.serve_static_assests = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
