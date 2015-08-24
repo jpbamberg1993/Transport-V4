47707c60d8ad04c63fdcb01f80566b6eb9af1429
John Paul Bamberg <jpbamberg1993@gmail.com>
2015-08-23 22:30:22 -0400

    test mail page loaded with user.first

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,2 +1,10 @@
-class ExampleMailer < ApplicationMailer
+# Not sure might be class ExampleMailer < ApplicationMailer::Base
+class ExampleMailer < ActionMailer::Base
+	default from: "jpbamberg1993@gmail.com"
+
+# might pass users
+	def sample_email(user)
+		@user = user
+		mail(to: @user.email, subject: 'FUCKING SHITTT!!!')
+	end
 end
